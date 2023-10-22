283d6df0978e68e98534a0af1f9d83e97a5fce80
ccaton64 <Charlescaton64@gmail.com>
2023-10-22 14:29:05 -0500

    fixed breaking, added wolf walk
    He spins tho

--- Project House/Assets/Scripts/Wolf.cs
+++ Project House/Assets/Scripts/Wolf.cs
@@ -6,9 +6,11 @@ public class Wolf : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator anim;
+    Vector3 lastPos;
     // Start is called before the first frame update
     void Start()
     {
+        lastPos = transform.position;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator> ();
     }
@@ -16,6 +18,17 @@ public class Wolf : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(charMoved() == true)
+        {
+            anim.SetBool("isWalking", true);
+        }
+    }
+
+    private bool charMoved()
+    {
+        var dispacement = transform.position - lastPos;
+        lastPos = transform.position;
+        Debug.Log("Moving");
+        return dispacement.magnitude > 0.001;
     }
 }
