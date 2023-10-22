641e0af0c5d6dfa3dc556c3184c2deefa9e518e7
ccaton64 <Charlescaton64@gmail.com>
2023-10-22 02:48:59 -0500

    tiny fix

--- Project House/Assets/Scripts/repairing.cs
+++ Project House/Assets/Scripts/repairing.cs
@@ -7,12 +7,15 @@ public class repairing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Make list of repairs
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //if at place to repair, stop for 5 seconds and then remove task
+        //if colides with phone ring, then set destination to phone location
     }
+
+    //public void remove from tasks
 }
