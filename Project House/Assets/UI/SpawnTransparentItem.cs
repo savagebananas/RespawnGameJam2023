d64f9d4a706d19b2b50a3602045c45a68f4323df
savagebananas <bruno7405@gmail.com>
2023-10-24 06:14:15 -0500

    UI Improvements and moltov bug fix

--- Project House/Assets/UI/SpawnTransparentItem.cs
+++ Project House/Assets/UI/SpawnTransparentItem.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnTransparentItem : MonoBehaviour
 {
+    public GameObject parentUIObject;
     public GameObject transparentItem;
 
     public void instantiateTransparentItem()
@@ -11,7 +13,8 @@ public class SpawnTransparentItem : MonoBehaviour
         if (!PlayerManager.mouseDragging)
         {
             Instantiate(transparentItem, Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10), Quaternion.identity);
-            Destroy(this.gameObject);
+            if (parentUIObject != null) Destroy(parentUIObject);
+            else Destroy(this.gameObject);
         }
     }
 }
