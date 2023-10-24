490421d43b0bdad24ae5c3dbc879f44d41994aaa
Anirudh <63116473+AnirudhJ77@users.noreply.github.com>
2023-10-23 22:11:22 -0500

    Rescan

--- Project House/Assets/Scripts/DraggableFurniture.cs
+++ Project House/Assets/Scripts/DraggableFurniture.cs
@@ -10,7 +10,7 @@ public class DraggableFurniture : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AstarPath.active.Scan();
     }
 
     // Update is called once per frame
