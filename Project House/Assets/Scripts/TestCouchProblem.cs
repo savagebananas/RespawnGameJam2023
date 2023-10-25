5589d7740901e7111c1e521c8202219722917fd3
Anirudh <63116473+AnirudhJ77@users.noreply.github.com>
2023-10-25 08:00:01 -0500

    Added pointed, fixed furniture, i am god

---
+++ Project House/Assets/Scripts/TestCouchProblem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCouchProblem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // if (Mathf.Abs(Time.time-10)<0.1) Debug.Log("Couch Testx : " + GameObject.Find("Couch (1)").GetComponent<>colliders.Count);
+
+    }
+}
