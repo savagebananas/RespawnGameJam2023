7b15f3f8019c397e94ea0574094f98bd0d10bb67
ccaton64 <Charlescaton64@gmail.com>
2023-10-24 23:58:44 -0500

    cam start

---
+++ Project House/Assets/limitCam.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class limitCam : MonoBehaviour
+{
+
+    public Vector3 minValues, maxValues;
+    //x, -17.73 7.34
+    //y, -14 11
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
