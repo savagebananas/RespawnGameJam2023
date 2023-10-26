2d12442bdcf9c0d5e4e3d15f23854a3fff389f5b
Anirudh <63116473+AnirudhJ77@users.noreply.github.com>
2023-10-26 00:42:08 -0500

    Fixed compile error

--- Project House/Assets/Scripts/SpawnGhost.cs
+++ Project House/Assets/Scripts/SpawnGhost.cs
@@ -30,7 +30,7 @@ public class SpawnGhost : MonoBehaviour
     private Vector3 getRandomPosition() {
         int temp = index;
         int loopCount = 0;
-        bool x = true
+        bool x = true;
         while ((temp==index||!locations[temp].GetComponent<GhostLocationRadius>().shouldSpawn||x)&&loopCount<50) {
             RacastHit2D hit = Physics2D.Raycast(locations[temp].transform.position, Vector2.zero);
             x = hit.collider!=null&&!hit.collider.gameObject.tag.Equals("phone")&&!hit.collider.gameObject.layer==5;
