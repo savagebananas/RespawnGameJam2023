5b33677106ba9919f4cd54acdae63c0c62322d66
ccaton64 <Charlescaton64@gmail.com>
2023-10-26 11:13:21 -0500

    sum sound

--- Project House/Assets/Sounds/AudioManager.cs
+++ Project House/Assets/Sounds/AudioManager.cs
@@ -29,4 +29,14 @@ public class AudioManager : MonoBehaviour
         }
         s.source.Play();
     }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            return;
+        }
+        s.source.Stop();
+    }
 }
