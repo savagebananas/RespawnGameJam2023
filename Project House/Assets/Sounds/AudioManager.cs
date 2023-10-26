bef55394d0a50a828845ad5a94c8f81c2f5bc038
ccaton64 <Charlescaton64@gmail.com>
2023-10-26 06:16:50 -0500

    sound start

---
+++ Project House/Assets/Sounds/AudioManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
