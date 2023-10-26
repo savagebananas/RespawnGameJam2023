944e49ffc0be1e28c97ebec96d1036c1a70d313d
savagebananas <bruno7405@gmail.com>
2023-10-26 15:04:30 -0500

    MAIN MENU FINISHED

--- Project House/Assets/Scripts/SceneTransition.cs
+++ Project House/Assets/Scripts/SceneTransition.cs
@@ -8,6 +8,10 @@ public class SceneTransition : MonoBehaviour
     private static string trName = "End";
     private static float time = 1f;
     public Animator transition;
+
+    public Animator ui1;
+    public Animator ui2;
+    public Animator ui3;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +28,14 @@ public class SceneTransition : MonoBehaviour
     }
     IEnumerator levelTransition() {
         transition.SetTrigger(trName);
+        if (ui1 != null) ui1.SetTrigger("fadeOut");
+        if (ui2 != null) ui2.SetTrigger("fadeOut");
+        if (ui3 != null) ui3.SetTrigger("fadeOut");
         yield return new WaitForSeconds(time);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        if(SceneManager.GetActiveScene().buildIndex != SceneManager.sceneCountInBuildSettings - 1) 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        else
+            SceneManager.LoadScene(0);
 
     }
 }
