8e7eb5048b801abcffbc9ab913a3aaec31b97a87
Anirudh <63116473+AnirudhJ77@users.noreply.github.com>
2023-10-26 01:36:11 -0500

    Added highlights to couch, changed layers, added object manager

---
+++ Project House/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    public GameObject highlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MouseHighlight.setLight(highlight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
