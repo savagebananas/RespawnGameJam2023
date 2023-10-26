32c673221a748141207dc755b01677ab92e17e57
ccaton64 <Charlescaton64@gmail.com>
2023-10-26 11:54:05 -0500

    FUCK

--- Project House/Assets/Prefabs/Molotov/Moli.cs
+++ Project House/Assets/Prefabs/Molotov/Moli.cs
@@ -24,6 +24,7 @@ public class Moli : MonoBehaviour
     void OnEnable()
     {
         StartCoroutine(Lifespan());
+        FindObjectOfType<AudioManager>().Play("Moli");
     }
 
     void OnTriggerEnter2D(Collider2D collision)
