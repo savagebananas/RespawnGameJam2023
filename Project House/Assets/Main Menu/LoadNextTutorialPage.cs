dca217eb2a3430018f22aef0a2cd494f9c71b8c1
savagebananas <bruno7405@gmail.com>
2023-12-25 20:38:42 -0600

    Fixed multiple tutorial opening bug

--- Project House/Assets/Main Menu/LoadNextTutorialPage.cs
+++ Project House/Assets/Main Menu/LoadNextTutorialPage.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class LoadNextTutorialPage : MonoBehaviour
 {
     public GameObject nextTutorialPage;
-    public void LoadNext()
+    public static bool tutorialOpen;
+
+    public void loadFirst()
     {
+        if (!tutorialOpen)
+        {
+            this.GetComponent<Animator>().SetTrigger("out");
+            if (nextTutorialPage != null) nextTutorialPage.GetComponent<Animator>().SetTrigger("in");
+            tutorialOpen = true;
+        }
+        else Debug.Log("tutorial panel already open");
+    }
+
+    public void loadNext()
+    {
+
         this.GetComponent<Animator>().SetTrigger("out");
         if (nextTutorialPage != null) nextTutorialPage.GetComponent<Animator>().SetTrigger("in");
-        
+        else tutorialOpen = false;
         
     }
 }
