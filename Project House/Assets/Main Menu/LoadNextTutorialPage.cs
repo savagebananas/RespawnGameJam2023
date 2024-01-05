cb18984607b1a9415f1c5ff514ff5f8aac492245
savagebananas <bruno7405@gmail.com>
2024-01-05 08:34:58 -0600

    Closing tutorial by repressing button + comments

--- Project House/Assets/Main Menu/LoadNextTutorialPage.cs
+++ Project House/Assets/Main Menu/LoadNextTutorialPage.cs
@@ -2,27 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Gets called when tutorial button and next tutorial page button is pressed
+ * Open and close tutorial + next page functionality
+ */
 public class LoadNextTutorialPage : MonoBehaviour
 {
+    private static GameObject currentTutorialPage;
     public GameObject nextTutorialPage;
     public static bool tutorialOpen;
 
+    /**
+     * Gets called when tutorial button is pressed.
+     * Opens tutorial if unopened, else close existing tutorial page
+     */
     public void loadFirst()
     {
-        if (!tutorialOpen)
+        if (!tutorialOpen) // open tutorial
         {
-            this.GetComponent<Animator>().SetTrigger("out");
-            if (nextTutorialPage != null) nextTutorialPage.GetComponent<Animator>().SetTrigger("in");
+            if (nextTutorialPage != null)
+            {
+                nextTutorialPage.GetComponent<Animator>().SetTrigger("in");
+                currentTutorialPage = nextTutorialPage as GameObject;
+            }
+            
             tutorialOpen = true;
         }
-        else Debug.Log("tutorial panel already open");
+        else // close tutorial
+        {
+            currentTutorialPage.GetComponent<Animator>().SetTrigger("out");
+            tutorialOpen = false;
+        }
     }
 
+    /**
+     * Loads next tutorial page
+     * Exits tutorial if none 
+     */
     public void loadNext()
     {
-
-        this.GetComponent<Animator>().SetTrigger("out");
-        if (nextTutorialPage != null) nextTutorialPage.GetComponent<Animator>().SetTrigger("in");
+        currentTutorialPage.GetComponent<Animator>().SetTrigger("out"); // remove current page
+        if (nextTutorialPage != null)
+        {
+            nextTutorialPage.GetComponent<Animator>().SetTrigger("in");
+            currentTutorialPage = nextTutorialPage as GameObject;
+        }
         else tutorialOpen = false;
         
     }
