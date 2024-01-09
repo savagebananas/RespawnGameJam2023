39fef047b7970dbbb023f57513f5113373ae22d7
savagebananas <bruno7405@gmail.com>
2024-01-09 03:13:32 -0600

    Revert "Got rid of empty unused scripts"
    This reverts commit ce8a65b2bd6a7b58a88c5459626a1b3678f066f5.

---
+++ Project House/Assets/Scripts/repairing.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class repairing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Make list of repairs
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if at place to repair, stop for 5 seconds and then remove task
+        //if colides with phone ring, then set destination to phone location
+    }
+
+    //public void remove from tasks
+}
